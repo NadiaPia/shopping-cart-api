bd2036a84493f091a1dd9aeb06555cc12f8201b2
NadiaPia <206i417@gmail.com>
2023-05-25 01:03:52 -0700

    Add validateToken middleware, complete router.get login handler

--- middlewares/AuthMiddlewares.js
+++ middlewares/AuthMiddlewares.js
@@ -1,5 +1,19 @@
 const { verify } = require("jsonwebtoken");
 
-/*const validateToken = (req, res, next) => {
+const validateToken = (req, res, next) => {
+    const accessToken = req.cookies['access-tokennn'];
+    if(!accessToken) return res.json({error: "User not logged in!"});
 
-}*/
+    try {
+        const validToken = verify(accessToken, process.env.SECRET_KEY);
+        req.user = validToken;
+        if (validToken) {
+            return next();
+        }
+    } catch (err) {
+        return res.json({error: err});
+    }
+
+}
+
+module.exports = {validateToken};
--- routes/Users.js
+++ routes/Users.js
@@ -2,16 +2,18 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
-const {sign} = require('jsonwebtoken');
+const { sign } = require('jsonwebtoken');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
+//const session = require("express-session");
+const {validateToken} = require("../middlewares/AuthMiddlewares")
+
 
 
 //    "/auth" = "/";
 
-router.post("/", async(req, res) => {
-    const { username, password} = req.body;
+router.post("/", async (req, res) => {
+    const { username, password } = req.body;
     bcrypt.hash(password, 10).then((hash) => {
         Users.create({
             username: username,
@@ -19,37 +21,37 @@ router.post("/", async(req, res) => {
         })
         res.json("------------USER IS SET UP TO THE DB---------------")
     }).catch((err) => {
-        if(err) {
-            res.status(400).json({error: err});
+        if (err) {
+            res.status(400).json({ error: err });
         }
     })
 })
 
-router.post("/login", async(req, res) => {
-    const {username, password} = req.body;
-    const user = await Users.findOne({where: {username: username}});
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await Users.findOne({ where: { username: username } });
 
-    if(!user) {
+    if (!user) {
         res.status(400).json("USER DOESN'T EXIST");
         return
-    }  
+    }
 
     const dbPassword = user.password;
     bcrypt.compare(password, dbPassword).then((match) => {
-        if(!match) {
-            res.status(400).json({error: "WRONG USERNAME AND PASSWORD COMBINATION"});
+        if (!match) {
+            res.status(400).json({ error: "WRONG USERNAME AND PASSWORD COMBINATION" });
         } else {
             // one way 
 
-            
-            const accessToken = sign({username: user.username, id: user.id}, process.env.SECRET_KEY)
+
+            const accessToken = sign({ username: user.username, id: user.id }, process.env.SECRET_KEY)
             //create a cookie in a browser:
             res.cookie("access-tokennn", accessToken, {
-                maxAge: 60*60*24*30*1000,
+                maxAge: 60 * 60 * 24 * 30 * 1000,
                 httpOnly: true, //will make our cookie not accessable to users: they cannot type in the console.log tab of a browser  somethig like: document.cookies.....               
             })
-            res.json({token: accessToken, username: user.username, id: user.id})
-            
+            res.json({ token: accessToken, username: user.username, id: user.id })
+
 
             /*another way*/
             //--------------if I need use express-session instead of JWT------------------
@@ -60,12 +62,13 @@ router.post("/login", async(req, res) => {
             console.log("req.session.userreq.session.userreq.session.user", req.session.user)
             res.json(user);
             */
-           //--------------------------------------------------------------------------------------------
+            //--------------------------------------------------------------------------------------------
         }
-    });    
+    });
 
 });
 
+//--------------if I need use express-session instead of JWT------------------
 /*router.get("/login", async(req, res) => {
     if(req.session.user) {
         res.send({loggedIn: true, user: req.session.user})
@@ -73,11 +76,22 @@ router.post("/login", async(req, res) => {
         res.send({loggedIn: false})
     }
 })*/
+//-------------------------------------------------------------------------------
 
+/*router.get("/login", async (req, res) => {
+    if(!req.cookies['access-tokennn']) {
+        res.status(401).json({error: "user is not logged in"});
+    } else {
+        console.log("if(req.cookies['access-tokennn'])", res)
+        res.json("user is logged in")
+    }
+    //console.log("////////////////////req.cookies.access-tokennn////////////////////////////////", req.cookies['access-tokennn'])
 
-router.get("/login", async(req, res) => {
-    console.log("////////////////////req////////////////////////////////", req.cookies)
+}); */
 
+router.get("/login", validateToken, (req, res) => {
+    res.json(req.user);
+    console.log("req.user", req.user)
 })
 
 
